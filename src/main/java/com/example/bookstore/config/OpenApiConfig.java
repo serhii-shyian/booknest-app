8ebf8ceeb15bb1de463218bd3e08ffbc40a6eba8
Serhii Shyian <serhii.shn@gmail.com>
2024-07-14 14:43:40 +0300

    1) In JwtAuthFilter class create constant TOKEN_PREFIX for incoming token 2) In OpenApiConfig class create constants for security scheme name and format

--- src/main/java/com/example/bookstore/config/OpenApiConfig.java
+++ src/main/java/com/example/bookstore/config/OpenApiConfig.java
@@ -9,15 +9,18 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+    private static final String SECURITY_SCHEME = "BearerAuth";
+    private static final String SCHEME_NAME = "bearer";
+    private static final String SCHEME_FORMAT = "JWT";
 
     @Bean
     public OpenAPI customOpenApi() {
         return new OpenAPI()
-                .components(new Components().addSecuritySchemes("BearerAuth",
+                .components(new Components().addSecuritySchemes(SECURITY_SCHEME,
                         new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")))
-                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"));
+                                .scheme(SCHEME_NAME)
+                                .bearerFormat(SCHEME_FORMAT)))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME));
     }
 }
